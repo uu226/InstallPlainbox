3b9d2a2e8b4cb387ed6e2c1d0ac45e3ffe5f89af
AlexWen <alex.wen@canonical.com>
2018-02-07 16:09:11 +0800

    add file

---
+++ SetupTestEnv.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+import os
+import commands
+import subprocess
+
+repo = '''
+deb http://ppa.launchpad.net/hardware-certification/public/ubuntu xenial main
+deb-src http://ppa.launchpad.net/hardware-certification/public/ubuntu xenial main
+deb https://ronglu-wen:8SC07M6lWDl11xM5Knnl@private-ppa.launchpad.net/oem-services-qa/ppa/ubuntu xenial main
+deb-src https://ronglu-wen:8SC07M6lWDl11xM5Knnl@private-ppa.launchpad.net/oem-services-qa/ppa/ubuntu xenial main
+'''
+
+packages = '''vim ssh plainbox python3-plainbox checkbox-gui checkbox-ng checkbox-ng-service plainbox-insecure-policy plainbox-provider-resource-generic plainbox-provider-checkbox python3-checkbox-ng python3-checkbox-support plainbox-provider-certification-client canonical-hw-collection iperf glmark2 render-bench sysstat phoronix-test-suite fwts obexftp gtkperf bonnie++ wmctrl expect ntpdate canonical-certification-client checkbox-oem-bug plainbox-provider-oem-sutton'''
+
+#TargetFile="/etc/apt/sources.list"
+TargetFile = "sources.list"
+
+with open(TargetFile,'a') as f:
+    f.write(repo)
+
+while True:
+    status = subprocess.call("""sudo apt-get update""",shell=True)
+    if status == 0:
+        for package in packages.split(" "):
+            print "***=== Prepare to install package %s ===***" %package 
+            subprocess.call('sudo apt-get install %s -y' %package,shell=True) 
+        break
+    else:
+        print "Import GPG key"
+        subprocess.call("""sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 101B58111FEADE10""",shell=True)
+        subprocess.call("""sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 17B878BE09D5DC1F""",shell=True)
+        continue
